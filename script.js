9e9c0e6a53b236d2d89819e94150cf2ab4b9478a
ALYAKOOB <seif.alyakoob@gmail.com>
2021-03-24 11:33:20 +0100

    update j.s fonction home

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+function submit(){
 const message =
   "We thank you for your request. Will contact you as soon as possible.";
 document
@@ -6,3 +7,20 @@ document
     event.preventDefault();
     alert(message);
   });
+}
+ 
+
+function extraire() {
+     character = message1.substring(I, I=I+1) ;
+     if ( character == "-" &&  message1.substr(I, 5) == "stop-" ) {
+          character = "" ;
+          I = I + 5;
+          }
+     titleSentence.innerHTML += character;
+     if ( I < message1.length ) setTimeout(extraire, 151);
+     }
+I = 0 ;
+message1 = "-stop-We Make-stop--stop--stop- Your Dream-stop--stop--stop- Come True";
+extraire();
+
+ 
